5e3cd9619571db83641071d84d0640ce4c3d3e4d
Ainesh Arumugam <ainesh01@gmail.com>
2021-02-13 22:02:08 -0800

    finally done :)

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,115 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Element functionality written in here
+
+
+  }
+
+  connectedCallback() {
+    this.loadDOMElements();
+  }
+
+
+  renderCart() {
+    document.getElementById('cart-count').innerText = window.localStorage.getItem('cartSize');
+  }
+
+
+  addToCart(buttonElement) {
+    window.localStorage.setItem(this.getAttribute('id'), true);
+    this.incrementCart();
+    buttonElement.textContent = 'Remove';
+    buttonElement.onclick = () => this.removeFromCart(buttonElement);
+  }
+
+  removeFromCart(buttonElement) {
+    window.localStorage.removeItem(this.getAttribute('id'));
+    this.decrementCart();
+    buttonElement.textContent = 'Add';
+    buttonElement.onclick = () => this.addToCart(buttonElement);
+  }
+
+  incrementCart() {
+    window.localStorage.setItem('cartSize', Number(window.localStorage.getItem('cartSize')) + 1);
+    this.renderCart();
+  }
+
+  decrementCart() {
+    window.localStorage.setItem('cartSize', Number(window.localStorage.getItem('cartSize')) - 1);
+    this.renderCart();
+  }
+
+  loadDOMElements() {
+    this.attachShadow({ mode: 'open' });
+    this.renderCart();
+
+    const listElement = this.createList();
+
+    this.createImage(listElement);
+    this.createTitle(listElement);
+    this.createPrice(listElement);
+    this.createButton(listElement);
+
+    const styleElement = this.addStyles();
+
+    this.shadowRoot.append(listElement, styleElement);
+  }
+
+
+
+  createList() {
+    const listElement = document.createElement('li')
+    listElement.className = 'product';
+    return listElement;
+  }
+
+  createImage(listElement) {
+    const imgElement = listElement.appendChild(document.createElement('img'));
+    imgElement.src = this.getAttribute('img');
+    return imgElement;
+  }
+
+  createTitle(listElement) {
+    const pTitleElement = listElement.appendChild(document.createElement('p'));
+    pTitleElement.className = 'title';
+    pTitleElement.textContent = this.getAttribute('title');
+    return pTitleElement;
+  }
+
+  createPrice(listElement) {
+    const pPriceElement = listElement.appendChild(document.createElement('p'));
+    pPriceElement.className = 'price';
+    pPriceElement.textContent = this.getAttribute('price');
+    return pPriceElement;
+  }
+
+  createButton(listElement) {
+    const buttonElement = listElement.appendChild(document.createElement('button'));
+    const itemInCart = window.localStorage.getItem(this.getAttribute('id')) === null ? false : true;
+    buttonElement.textContent = itemInCart ? 'Remove' : 'Add';
+
+
+    buttonElement.onclick = () => itemInCart ? this.removeFromCart(buttonElement) : this.addToCart(buttonElement);
+
+    return buttonElement;
+  }
+
+
+
+  addStyles() {
+    const styleElement = document.createElement('link')
+    styleElement.setAttribute('rel', 'stylesheet');
+    styleElement.setAttribute('href', './styles/styles.css');
+    return styleElement;
+  }
+
 }
+
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,24 @@ window.addEventListener('DOMContentLoaded', async () => {
   console.log(productJson);
   window.localStorage.setItem('products', JSON.stringify(productJson));
   console.log(window.localStorage.getItem('products'));
+  const productList = JSON.parse(window.localStorage.getItem('products'));
+  const productElementList = document.getElementById('product-list');
+  let product = '';
+  productList.forEach((product) => {
+    const productElement = document.createElement('product-item');
+    productElement.setAttribute('id', product.id);
+    productElement.setAttribute('img', product.image);
+    productElement.setAttribute('title', product.title);
+    productElement.setAttribute('price', product.price);
+    productElementList.appendChild(productElement);
+  });
+  if (!window.localStorage.getItem('cartSize')) {
+    window.localStorage.setItem('cartSize', 0)
+    console.log(window.localStorage.getItem('cartSize'));
+  }
+
+
+
 
 });
 
