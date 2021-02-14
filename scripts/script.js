ced019b88983d5b71b95db116b08a748e3917e53
Ainesh Arumugam <ainesh01@gmail.com>
2021-02-13 20:11:56 -0800

    added the fetch and storing fucntiodasjd

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,14 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+
+window.addEventListener('DOMContentLoaded', async () => {
+
+  const response = await fetch('https://fakestoreapi.com/products');
+  console.log(response);
+  const productJson = await response.json();
+  console.log(productJson);
+  window.localStorage.setItem('products', JSON.stringify(productJson));
+  console.log(window.localStorage.getItem('products'));
+
 });
+
